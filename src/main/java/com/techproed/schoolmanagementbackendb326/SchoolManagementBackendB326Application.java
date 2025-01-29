c0451718760066df69aafe364ba68e56a4197b45
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2025-01-29 22:51:45 +0100

    first commit

---
+++ src/main/java/com/techproed/schoolmanagementbackendb326/SchoolManagementBackendB326Application.java
@@ -0,0 +1,13 @@
+package com.techproed.schoolmanagementbackendb326;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolManagementBackendB326Application {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SchoolManagementBackendB326Application.class, args);
+  }
+
+}
