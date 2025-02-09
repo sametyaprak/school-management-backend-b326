ae28165f37b85f42137f85a416b7a705148599bd
Samet Yaprak <syaprak@indracompany.com>
2025-02-09 20:41:55 +0100

    success and error messages

--- src/main/java/com/techproed/schoolmanagementbackendb326/repository/user/UserRepository.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/repository/user/UserRepository.java
@@ -1,5 +1,21 @@
 package com.techproed.schoolmanagementbackendb326.repository.user;
 
-public interface UserRepository {
+import com.techproed.schoolmanagementbackendb326.entity.concretes.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+  boolean existsByUsername(String username);
+
+  boolean existsByEmail(String email);
+
+  boolean existsBySsn(String ssn);
+
+  boolean existsByPhoneNumber(String phoneNumber);
+
+
 
 }
