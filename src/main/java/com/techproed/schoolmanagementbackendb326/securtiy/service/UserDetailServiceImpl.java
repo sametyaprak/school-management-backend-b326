2f5deaa40bc2ee01f0824719e1964414a1c65a0e
samet yaprak <syaprak@indracompany.com>
2025-02-11 21:42:13 +0100

    Jwt utils

--- src/main/java/com/techproed/schoolmanagementbackendb326/securtiy/service/UserDetailServiceImpl.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/securtiy/service/UserDetailServiceImpl.java
@@ -1,8 +1,9 @@
 package com.techproed.schoolmanagementbackendb326.securtiy.service;
 
+import com.techproed.schoolmanagementbackendb326.entity.concretes.user.User;
 import com.techproed.schoolmanagementbackendb326.service.helper.MethodHelper;
 import lombok.AllArgsConstructor;
-import org.springframework.security.core.userdetails.User;
+
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,6 +17,11 @@ public class UserDetailServiceImpl implements UserDetailsService {
 
   @Override
   public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = methodHelper.
+    User user = methodHelper.loadByUsername(username);
+    return new UserDetailsImpl(
+        user.getId(),
+        user.getUsername(),
+        user.getPassword(),
+        user.getUserRole().getRoleType().getName());
   }
 }
