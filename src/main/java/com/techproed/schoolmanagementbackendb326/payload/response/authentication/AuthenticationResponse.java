c0bac046e64fca735c863fb15350dfed681d8a41
Samet Yaprak <syaprak@indracompany.com>
2025-02-12 21:27:13 +0100

    login controller

--- src/main/java/com/techproed/schoolmanagementbackendb326/payload/response/authentication/AuthenticationResponse.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/payload/response/authentication/AuthenticationResponse.java
@@ -2,12 +2,14 @@ package com.techproed.schoolmanagementbackendb326.payload.response.authenticatio
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class AuthenticationResponse {
 
