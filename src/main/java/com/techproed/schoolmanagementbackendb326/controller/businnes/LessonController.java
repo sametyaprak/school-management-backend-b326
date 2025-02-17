4a3e7de33bb9bf30eb4aa031461c8ed1da1d6e51
Samet Yaprak <syaprak@indracompany.com>
2025-02-17 21:31:15 +0100

    lesson request

---
+++ src/main/java/com/techproed/schoolmanagementbackendb326/controller/businnes/LessonController.java
@@ -0,0 +1,12 @@
+package com.techproed.schoolmanagementbackendb326.controller.businnes;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/lesson")
+@RequiredArgsConstructor
+public class LessonController {
+
+}
