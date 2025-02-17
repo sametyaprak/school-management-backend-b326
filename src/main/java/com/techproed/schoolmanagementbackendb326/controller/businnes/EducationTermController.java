7a710f01170cab91592c44d4c171b0ed843e364c
Samet Yaprak <syaprak@indracompany.com>
2025-02-17 21:18:21 +0100

    EducationTerm delete

--- src/main/java/com/techproed/schoolmanagementbackendb326/controller/businnes/EducationTermController.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/controller/businnes/EducationTermController.java
@@ -5,17 +5,21 @@ import com.techproed.schoolmanagementbackendb326.payload.response.business.Educa
 import com.techproed.schoolmanagementbackendb326.payload.response.business.ResponseMessage;
 import com.techproed.schoolmanagementbackendb326.service.businnes.EducationTermService;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -49,6 +53,35 @@ public class EducationTermController {
     //return educationTermService.getAllEducationTerms();
     return null;
   }
+
+
+  //TODO
+  // esra
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher')")
+  @GetMapping("/{educationTermId}")
+  public EducationTermResponse getEducationTerm(Long educationTermId) {
+    //return educationTermService.getEducationTermById(educationTermId);
+    return null;
+  }
+
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher')")
+  @GetMapping("/getByPage")
+  public Page<EducationTermResponse>getByPage(
+      @RequestParam(value = "page",defaultValue = "0") int page,
+      @RequestParam (value = "size",defaultValue = "10") int size,
+      @RequestParam (value = "sort",defaultValue = "term") String sort,
+      @RequestParam (value = "type",defaultValue = "desc") String type) {
+    return educationTermService.getByPage(page,size,sort,type);
+  }
+
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher')")
+  @DeleteMapping("/delete/{educationTermId}")
+  public ResponseMessage deleteEducationTerm(@PathVariable Long educationTermId) {
+    return educationTermService.deleteById(educationTermId);
+  }
+
+
+
 
 
 
