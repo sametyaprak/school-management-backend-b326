a20f004c0aa8baa69aecec542dfd8cc9961a86d0
samet yaprak <syaprak@indracompany.com>
2025-02-18 20:24:13 +0100

    end of session

--- src/main/java/com/techproed/schoolmanagementbackendb326/service/businnes/LessonService.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/service/businnes/LessonService.java
@@ -1,25 +1,51 @@
 package com.techproed.schoolmanagementbackendb326.service.businnes;
 
+import com.techproed.schoolmanagementbackendb326.entity.concretes.business.Lesson;
+import com.techproed.schoolmanagementbackendb326.exception.ConflictException;
+import com.techproed.schoolmanagementbackendb326.payload.mappers.LessonMapper;
+import com.techproed.schoolmanagementbackendb326.payload.messages.ErrorMessages;
+import com.techproed.schoolmanagementbackendb326.payload.messages.SuccessMessages;
 import com.techproed.schoolmanagementbackendb326.payload.request.business.LessonRequest;
 import com.techproed.schoolmanagementbackendb326.payload.response.business.LessonResponse;
 import com.techproed.schoolmanagementbackendb326.payload.response.business.ResponseMessage;
+import com.techproed.schoolmanagementbackendb326.repository.businnes.LessonRepository;
 import javax.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class LessonService {
+
+  private final LessonRepository lessonRepository;
+  private final LessonMapper lessonMapper;
 
   public ResponseMessage<LessonResponse> saveLesson(@Valid LessonRequest lessonRequest) {
   //validate - lesson name must be unique
+  isLessonExistByName(lessonRequest.getLessonName());
+  //map DTO to Entity
+    Lesson lesson = lessonMapper.mapLessonRequestToLesson(lessonRequest);
+    Lesson savedLesson = lessonRepository.save(lesson);
 
-
+    return ResponseMessage.<LessonResponse>builder()
+        .returnBody(lessonMapper.mapLessonToLessonResponse(savedLesson))
+        .httpStatus(HttpStatus.CREATED)
+        .message(SuccessMessages.LESSON_SAVE)
+        .build();
   }
 
 
 
 
-  private void isLessonExistByName(String lessonName) {
 
+
+
+
+  private void isLessonExistByName(String lessonName) {
+    if(lessonRepository.findByLessonNameEqualsIgnoreCase(lessonName).isPresent()) {
+      throw new ConflictException(String.format(ErrorMessages.ALREADY_CREATED_LESSON_MESSAGE,lessonName));
+    }
   }
 
 
