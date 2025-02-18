174de841717729b67ab0d4be2d91532b1fec7910
samet yaprak <syaprak@indracompany.com>
2025-02-18 19:23:01 +0100

    lesson save

--- src/main/java/com/techproed/schoolmanagementbackendb326/controller/businnes/LessonController.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/controller/businnes/LessonController.java
@@ -1,6 +1,14 @@
 package com.techproed.schoolmanagementbackendb326.controller.businnes;
 
+import com.techproed.schoolmanagementbackendb326.payload.request.business.LessonRequest;
+import com.techproed.schoolmanagementbackendb326.payload.response.business.LessonResponse;
+import com.techproed.schoolmanagementbackendb326.payload.response.business.ResponseMessage;
+import com.techproed.schoolmanagementbackendb326.service.businnes.LessonService;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/lesson")
 @RequiredArgsConstructor
 public class LessonController {
+
+  private final LessonService lessonService;
+
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean')")
+  @PostMapping("/save")
+  public ResponseMessage<LessonResponse>saveLesson(
+      @RequestBody @Valid LessonRequest lessonRequest) {
+    return lessonService.saveLesson(lessonRequest);
+  }
 
 }
--- src/main/java/com/techproed/schoolmanagementbackendb326/repository/businnes/LessonRepository.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/repository/businnes/LessonRepository.java
@@ -1,10 +1,16 @@
 package com.techproed.schoolmanagementbackendb326.repository.businnes;
 
 import com.techproed.schoolmanagementbackendb326.entity.concretes.business.Lesson;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LessonRepository extends JpaRepository<Lesson, Long> {
+
+  //@Query("select l from Lesson l where l.lessonName = :lessonName")
+  Optional<Lesson> findByLessonNameEqualsIgnoreCase(String lessonName);
+
 
 }
