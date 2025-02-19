11cd35063799afe389df8b71ffedcbf8c7f470d8
Yasar TAY <yasartay@gmail.com>
2025-02-19 23:15:49 +0100

    EducationTermMapper interface implemented in terms of MapStruct

--- src/main/java/com/techproed/schoolmanagementbackendb326/payload/mappers/EducationTermMapper.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/payload/mappers/EducationTermMapper.java
@@ -3,42 +3,13 @@ package com.techproed.schoolmanagementbackendb326.payload.mappers;
 import com.techproed.schoolmanagementbackendb326.entity.concretes.business.EducationTerm;
 import com.techproed.schoolmanagementbackendb326.payload.request.business.EducationTermRequest;
 import com.techproed.schoolmanagementbackendb326.payload.response.business.EducationTermResponse;
-import org.springframework.stereotype.Component;
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
 
-@Component
-public class EducationTermMapper {
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+public interface EducationTermMapper {
 
-    /**
-     *
-     * @param educationTermRequest DTO from Postman or FE
-     * @return  EducationTerm Entity
-     */
-    public EducationTerm mapEducationTermRequestToEducationTerm(EducationTermRequest educationTermRequest){
-        return EducationTerm.builder()
-                .term(educationTermRequest.getTerm())
-                .startDate(educationTermRequest.getStartDate())
-                .endDate(educationTermRequest.getEndDate())
-                .lastRegistrationDate(educationTermRequest.getLastRegistrationDate())
-                .build();
-    }
+    EducationTerm mapEducationTermRequestToEducationTerm(EducationTermRequest educationTermRequest);
 
-    /**
-     *
-     * @param educationTerm Entity fetched from DB
-     * @return EducationTermResponse DTO
-     */
-    public EducationTermResponse mapEducationTermToEducationTermResponse(EducationTerm educationTerm){
-        return EducationTermResponse.builder()
-                .id(educationTerm.getId())
-                .term(educationTerm.getTerm())
-                .startDate(educationTerm.getStartDate())
-                .endDate(educationTerm.getEndDate())
-                .lastRegistrationDate(educationTerm.getLastRegistrationDate())
-                .build();
-    }
-
-
-
-
-
+    EducationTermResponse mapEducationTermToEducationTermResponse(EducationTerm educationTerm);
 }
