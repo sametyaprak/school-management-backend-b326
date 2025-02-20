98f21adb414691730782f6cb5353bcf2ae91e57a
Samet Yaprak <syaprak@indracompany.com>
2025-02-20 21:22:33 +0100

    getAllUnassigned lessonPrograms

--- src/main/java/com/techproed/schoolmanagementbackendb326/controller/businnes/LessonProgramController.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/controller/businnes/LessonProgramController.java
@@ -5,9 +5,12 @@ import com.techproed.schoolmanagementbackendb326.payload.response.business.Lesso
 import com.techproed.schoolmanagementbackendb326.payload.response.business.ResponseMessage;
 import com.techproed.schoolmanagementbackendb326.repository.businnes.LessonProgramRepository;
 import com.techproed.schoolmanagementbackendb326.service.businnes.LessonProgramService;
+import java.util.List;
 import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,6 +30,31 @@ public class LessonProgramController {
       @RequestBody @Valid LessonProgramRequest lessonProgramRequest) {
     return lessonProgramService.saveLessonProgram(lessonProgramRequest);
   }
+
+  //TODO KEMAL
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher','Student')")
+  @GetMapping("/getAll")
+  public List<LessonProgramResponse> getAllLessonPrograms(){
+    //return lessonProgramService.getAllLessonPrograms();
+    return null;
+  }
+
+  //TODO LEVEN
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher','Student')")
+  @GetMapping("/getLessonProgram/{id}")
+  public LessonProgramResponse getLessonProgramById(@PathVariable Long id){
+    //return lessonProgramService.findById(id);
+    return null;
+  }
+
+  @PreAuthorize("hasAnyAuthority('Admin','Dean','ViceDean','Teacher','Student')")
+  @GetMapping("/getAllUnassigned")
+  public List<LessonProgramResponse>getAllUnassignedLessonPrograms(){
+    return lessonProgramService.getAllUnassigned();
+  }
+
+
+
 
 
 }
--- src/main/java/com/techproed/schoolmanagementbackendb326/repository/businnes/LessonProgramRepository.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/repository/businnes/LessonProgramRepository.java
@@ -1,13 +1,16 @@
 package com.techproed.schoolmanagementbackendb326.repository.businnes;
 
 import com.techproed.schoolmanagementbackendb326.entity.concretes.business.LessonProgram;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface LessonProgramRepository extends JpaRepository<LessonProgram, Long> {
 
+    List<LessonProgram>findByUsers_IdNull();
 
+    List<LessonProgram>findByUsers_IdNotNull();
 
 
 }
