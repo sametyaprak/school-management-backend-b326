5d5b75ca57abb51917befd62ba39e01e224c265d
Yasar TAY <yasartay@gmail.com>
2025-02-20 11:41:23 +0100

    Added two more parameter into the @Mapper Annotation.EducationTermMapper can be used now for partial update operations.

--- src/main/java/com/techproed/schoolmanagementbackendb326/payload/mappers/EducationTermMapper.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/payload/mappers/EducationTermMapper.java
@@ -5,8 +5,14 @@ import com.techproed.schoolmanagementbackendb326.payload.request.business.Educat
 import com.techproed.schoolmanagementbackendb326.payload.response.business.EducationTermResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
+import org.mapstruct.NullValueCheckStrategy;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING)
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
+        //with this parameter, MapStruct will always check source properties if they have null value or not.
+        nullValueCheckStrategy = NullValueCheckStrategy.ALWAYS,
+        //If a source bean property equals null, the target bean property will be ignored and retain its existing value. So, we will be able to perform partial update.
+        nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
 public interface EducationTermMapper {
 
     EducationTerm mapEducationTermRequestToEducationTerm(EducationTermRequest educationTermRequest);
