508e8edb6aa0ad5478dc7afaa85686836392e003
Samet Yaprak <syaprak@indracompany.com>
2025-02-23 19:21:51 +0100

    StudentRequest payload

---
+++ src/main/java/com/techproed/schoolmanagementbackendb326/controller/user/StudentController.java
@@ -0,0 +1,12 @@
+package com.techproed.schoolmanagementbackendb326.controller.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/student")
+@RequiredArgsConstructor
+public class StudentController {
+
+}
---
+++ src/main/java/com/techproed/schoolmanagementbackendb326/service/user/StudentService.java
@@ -0,0 +1,10 @@
+package com.techproed.schoolmanagementbackendb326.service.user;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class StudentService {
+
+}
