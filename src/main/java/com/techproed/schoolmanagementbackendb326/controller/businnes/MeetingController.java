b049dbf8220a0bb1a36fc2249956dc573b6246a2
Samet Yaprak <syaprak@indracompany.com>
2025-02-25 21:36:55 +0100

    meeting helper

--- src/main/java/com/techproed/schoolmanagementbackendb326/controller/businnes/MeetingController.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/controller/businnes/MeetingController.java
@@ -1,11 +1,15 @@
 package com.techproed.schoolmanagementbackendb326.controller.businnes;
 
+import com.techproed.schoolmanagementbackendb326.payload.request.business.MeetingRequest;
+import com.techproed.schoolmanagementbackendb326.payload.response.business.MeetingResponse;
 import com.techproed.schoolmanagementbackendb326.payload.response.business.ResponseMessage;
 import com.techproed.schoolmanagementbackendb326.service.businnes.MeetingService;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +22,10 @@ public class MeetingController {
 
   @PreAuthorize("hasAnyAuthority('Teacher')")
   @PostMapping("/save")
-  public ResponseMessage<>saveMeeting(){
-
+  public ResponseMessage<MeetingResponse>saveMeeting(
+      HttpServletRequest httpServletRequest,
+      @RequestBody @Valid MeetingRequest meetingRequest){
+    return meetingService.save(httpServletRequest,meetingRequest);
   }
 
 }
--- src/main/java/com/techproed/schoolmanagementbackendb326/service/businnes/MeetingService.java
+++ src/main/java/com/techproed/schoolmanagementbackendb326/service/businnes/MeetingService.java
@@ -1,8 +1,38 @@
 package com.techproed.schoolmanagementbackendb326.service.businnes;
 
+import com.techproed.schoolmanagementbackendb326.entity.concretes.user.User;
+import com.techproed.schoolmanagementbackendb326.payload.mappers.MeetingMapper;
+import com.techproed.schoolmanagementbackendb326.payload.request.business.MeetingRequest;
+import com.techproed.schoolmanagementbackendb326.payload.response.business.MeetingResponse;
+import com.techproed.schoolmanagementbackendb326.payload.response.business.ResponseMessage;
+import com.techproed.schoolmanagementbackendb326.repository.businnes.MeetingRepository;
+import com.techproed.schoolmanagementbackendb326.service.helper.MethodHelper;
+import com.techproed.schoolmanagementbackendb326.service.helper.PageableHelper;
+import com.techproed.schoolmanagementbackendb326.service.validator.TimeValidator;
+import javax.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class MeetingService {
 
+  private final MeetingRepository meetingRepository;
+  private final TimeValidator timeValidator;
+  private final MeetingMapper meetingMapper;
+  private final PageableHelper pageableHelper;
+  private final MethodHelper methodHelper;
+
+
+  public ResponseMessage<MeetingResponse> save(HttpServletRequest httpServletRequest,
+      MeetingRequest meetingRequest) {
+    String username = (String) httpServletRequest.getAttribute("username");
+    User teacher = methodHelper.loadByUsername(username);
+    methodHelper.checkIsAdvisor(teacher);
+    timeValidator.checkStartIsBeforeStop(
+        meetingRequest.getStartTime(),
+        meetingRequest.getStopTime());
+
+
+  }
 }
